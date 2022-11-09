703f1fb340a1bd31e4bea6fdc4df6e9718abeac0
לירז גנון <lirazganon@gmail.com>
2022-11-09 13:55:59 +0200

    getrandomint

--- services/util.service.js
+++ services/util.service.js
@@ -1,12 +1,19 @@
 export const utilService = {
     makeId,
-    saveToStorage
+    saveToStorage,
+    getRandomInt
 
 }
 
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value) || null)
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
 
 function makeId(length = 5) {
     var txt = ''
