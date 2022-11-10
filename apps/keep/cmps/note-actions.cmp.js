29ba111d73b456b2737078340ca4b038c5c0140b
Nadav Shambi <113799531+NadavShambi@users.noreply.github.com>
2022-11-10 14:13:21 +0200

    can add imges now

--- apps/keep/cmps/note-actions.cmp.js
+++ apps/keep/cmps/note-actions.cmp.js
@@ -3,20 +3,20 @@ export default {
 <section>
 <div class="actions">
         <div class="action delete-note" title="delete">
-            <img src="../../../assets/img/trash.png" alt="" @click="deleteNote" />
+            <img src="../../assets/img/trash.png" alt="" @click="deleteNote" />
         </div>
         <div class="action edit-note">
-            <img src="../../../assets/img/editing.png" alt="" />
+            <img src="../../assets/img/editing.png" alt="" />
         </div>
         <div class="action send-note">
-            <img src="../../../assets/img/envelope.png" alt="" />
+            <img src="../../assets/img/envelope.png" alt="" />
         </div>
         <div class="action color-note">
-            <img src="../../../assets/img/paint.png"/>
+            <img src="../../assets/img/paint.png"/>
             
         </div>
         <div class="action edit-note">
-            <img src="../../../assets/img/pin.png" alt="" />
+            <img src="../../assets/img/pin.png" alt="" />
         </div>
     </div>
 </section>
