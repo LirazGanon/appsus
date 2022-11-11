362c3e25b4f584289b5e79f91da7b5450f6d5496
לירז גנון <lirazganon@gmail.com>
2022-11-11 17:36:47 +0200

    lable and layout

--- apps/mail/cmps/mail-preview.cmp.js
+++ apps/mail/cmps/mail-preview.cmp.js
@@ -8,17 +8,18 @@ export default {
         <i v-if="!mail.IsStarred" class="fa-regular fa-star fade" @click.stop="setStarred(mail)" ></i>
         <i v-else class="fa-solid fa-star" @click.stop="setStarred(mail)" class="starred"></i>
      
-        <h3 class="clean-space sender text-overflow">{{ senderName }}</h3>
+        <h3 class="clean-space sender text-overflow" :class="{'un-bold':mail.isRead}" >{{ senderName }}</h3>
         <section class="content">
-            <h3 class="title clean-space">{{ mail.subject }}</h3>
+            <h3 class="title clean-space" :class="{'un-bold':mail.isRead}"  >{{ mail.subject }}</h3>
             <h3 class="desc clean-space">{{ mail.body}} </h3>
         </section>
         <h3 class="clean-space sentAt">{{ formattedTime}} </h3>
 
             <section class="actions" :class="{read:mail.isRead}">
-            <button @click.stop="removeMail(mail.id)"><i class="fa-regular fa-trash-can"></i></button>
-            <button @click.stop="toggleRead(mail)"><i class="fa-regular fa-envelope"></i></button>
-            <button @click.stop=""><i class="fa-regular fa-clock"></i></button>
+            <button @click.stop="removeMail(mail.id)" title="Delete"><i class="fa-regular fa-trash-can"></i></button>
+            <button v-if="mail.isRead" @click.stop="toggleRead(mail)" title="Mark as unread"><i class="fa-regular fa-envelope"></i></button>
+            <button v-else @click.stop="toggleRead(mail)" title="Mark as read"><i class="fa-regular fa-envelope-open"></i></button>
+            <button @click.stop="" title="Don't Know what this button do"><i class="fa-regular fa-clock"></i></button>
         </section>
         </article>
     `,methods: {
