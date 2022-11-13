8131b8171bd06311fde50a89fdc28279e5ead4be
Nadav Shambi <113799531+NadavShambi@users.noreply.github.com>
2022-11-13 08:28:21 +0200

    favicon

--- cmps/pallette-picker.cmp.js
+++ cmps/pallette-picker.cmp.js
@@ -33,7 +33,7 @@ export default {
                 {
                     var: '--nav-bg',
                     color: '#333'
-                }
+                },
                 ],
 
                 [{
